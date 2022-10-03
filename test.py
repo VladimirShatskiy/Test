0dc8c2eb579161c17b5efc842cc3cafb43ab35dd
Vladimir Shatskiy <shatskiy.v@inbox.ru>
2022-10-03 19:52:17 +0300

    Добавил новый файл

---
+++ test.py
@@ -0,0 +1,26 @@
+from re import X
+
+
+for num in range(10):
+    print(num)
+
+i = 0
+x = 1
+while i < 100:
+    x += 12
+    i += 1
+print(x)
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
